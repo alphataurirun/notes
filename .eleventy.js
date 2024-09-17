f588826a7567a5acc7ffe6ebb978ebd280a88c7c
alphataurirun <frantisek@alphatauri.run>
2024-09-17 20:38:22 +0200

    initial commit

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,15 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig){
 
     // addPassThroughCopy method creates a file/folder copy in the output directory
     eleventyConfig.addPassthroughCopy("src/assets/css");
     eleventyConfig.addPassthroughCopy("src/assets/img");
     eleventyConfig.addPassthroughCopy("src/assets/js");
+
+    //shortcodes
+    // get the current year
+    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
     return {
     // markdown files, data files and html files will be processed by Nunjucks
